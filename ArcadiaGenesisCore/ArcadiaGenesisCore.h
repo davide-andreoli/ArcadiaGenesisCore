635f2e38058847bbecf79bf6bbdf532f2a7dd059
Davide Andreoli <davide.andreoli@icloud.com>
2024-07-26 19:31:08 +0200

    Initial Commit

---
+++ ArcadiaGenesisCore/ArcadiaGenesisCore.h
@@ -0,0 +1,18 @@
+//
+//  ArcadiaGenesisCore.h
+//  ArcadiaGenesisCore
+//
+//  Created by Davide Andreoli on 26/07/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ArcadiaGenesisCore.
+FOUNDATION_EXPORT double ArcadiaGenesisCoreVersionNumber;
+
+//! Project version string for ArcadiaGenesisCore.
+FOUNDATION_EXPORT const unsigned char ArcadiaGenesisCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ArcadiaGenesisCore/PublicHeader.h>
+
+
